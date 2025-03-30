0d962cb80b53ade3744324cbbc9817910e99b0ae
Worker2000 <faceitlolaw@gmail.com>
2025-03-30 12:42:21 +0300

    fixed lazy agai

--- src/app/components/ProductCard.js
+++ src/app/components/ProductCard.js
@@ -1,27 +1,18 @@
 import React from "react";
 import Link from "next/link";
-import Image from "next/image";
-
 const ProductCard = ({ id, name, image, price, description }) => {
   return (
+    
     <div className="productCard">
       <Link className="link" href={`/${id}`}>
-        <div className="productImageWrapper">
-          <Image
-            className="productImage"
-            src={image}
-            alt={name}
-            
-            loading="lazy" // по умолчанию lazy loading
-            placeholder="blur" // добавление эффекта размытия до загрузки
-            blurDataURL="data:image/png;base64,..." // опционально: маленькое размытие
-          />
-        </div>
-        <div className="productInfo">
-          <div className="productPrice">{price} ₽</div>
-          <div className="productName">{name}</div>
-          <div className="inStock">• В наличии</div>
-        </div>
+      <img className="productImage" src={image} alt={name}  loading="lazy" />
+      <div className="productInfo">
+      <div className="productPrice">{price} ₽ </div>
+      <div className="productName">{name}</div>
+      <div className="inStock"> • В наличии</div>
+      </div>
+      
+      
       </Link>
     </div>
   );
